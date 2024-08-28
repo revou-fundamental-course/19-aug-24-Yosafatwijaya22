a89cc94408190cd16f94b4f7f5b1011d6971d1a9
yosafatwijaya7 <yosawijaya7@gmail.com>
2024-08-29 00:14:25 +0700

    updatecode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// ini javascript
+function validateForm() {
+  const name = document.forms['message-form']['full-name'].value;
+  const birthDate = document.forms['message-form']['birth-date'].value;
+  const gender = document.forms['message-form']['gender'].value;
+  const messages = document.forms['message-form']['messages'].value;
+
+  if (name == '' || birthDate == '' || gender == '' || messages == '') {
+    alert('Tidak boleh kosong');
+    return false;
+  }
+
+  setSenderUI(name, birthDate, gender, messages);
+
+  return false;
+}
